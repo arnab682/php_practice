d4f826b4e034c9e58932b4037ccd5dabd88743cd
Arnab682 <arnab.das682@gmail.com>
2023-09-14 19:51:59 +0600

    php practice

---
+++ class-1/variable.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 10;
+$b = 11.23;
+$c = "txt";
+$d = 'dd';
+$e = `tt`;
+
+echo $a. '<br/>';
+print_r($c.'<br/>');
+printf($b.'<br/>');
+//printf $b;
+//print_r $c;
+echo "Hey!";
---
+++ class-2/personal_info.php
@@ -0,0 +1,10 @@
+<?php
+//Create a PHP file named personal_info.php 
+##that displays your personal information using variables and the echo statement. 
+//Include your name, age, country, and a brief introduction.
+$name = 'Arnab';
+$age = 29;
+$country = 'Bangladesh';
+$info = "I am $name. I am $age old. I am from $country. I am working in Azim Group.";
+
+echo $info;
